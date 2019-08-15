bec89513fb34143a45b7b523918c2054d1b92ff7
Renee Sims <info@reneesoul.com>
2019-08-15 17:50:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word.match(/\b[aeiouAEIUO]/)
+    true 
+  elsif word.match(/\b[^aeiouAEIUO]/)
+    false 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  uning = []
+# binding.pry  
+  if text.scan(/\w+[un]/) && text.scan(/\w+ing/)
+    uning << text
+  end
+  uning
+  
 end
 
 def words_five_letters_long(text)
+  text.scan(/\w{5}/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
+  # binding.pry
+  if phone.scan(/\d{7,10}/)
+    true
+  end
 
 end
